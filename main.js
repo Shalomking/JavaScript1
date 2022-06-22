92fa6e452385901a925f75f5cb6e23027c6733b6
Shalomking <favour912@gmail.com>
2022-06-22 03:15:17 +0200

    This is Zuri Task

---
+++ main.js
@@ -0,0 +1,8 @@
+var NameSlot = document.querySelector(".myname");
+NameSlot.innerHTML = "My name is Shalom Onyeukwu";
+
+var MyHeight = document.querySelector(".myheight");
+MyHeight.innerHTML = "I am 5.1ft Tall in height";
+
+var MyCountry = document.querySelector(".mycountry");
+MyCountry.innerHTML = "Nigeria";
